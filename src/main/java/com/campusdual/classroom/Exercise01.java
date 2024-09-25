af1084a8985b1015d3129d36b61f9598f7ed1f44
hiraryw64 <hiraryw64@gmail.com>
2024-09-25 11:34:41 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -4,6 +4,6 @@ public class Exercise01 {
 
     public static void main(String[] args) {
         //TODO → Write: System.out.println("Hello, World!");
-        System.out.println("¡Hola, mundo!");
+        System.out.println("Hello, World!");
     }
 }
